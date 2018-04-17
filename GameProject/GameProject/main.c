8f948b140cb9f63c41154e6366485cb06b255b15
Dumitru Muntean <261824@via.dk>
2018-04-17 10:06:36 +0200

    Empty project created

---
+++ GameProject/GameProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * GameProject.c
+ *
+ * Created: 17/04/2018 10:05:54
+ * Author : Dima
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
