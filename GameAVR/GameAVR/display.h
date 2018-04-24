23ebebbf2d14c91ced8ace4199442c3e763af728
Andrew Ngwira <260008@via.dk>
2018-04-24 11:49:21 +0200

    display.c and display.h

--- GameAVR/GameAVR/display.h
+++ GameAVR/GameAVR/display.h
@@ -9,5 +9,17 @@
 #define DISPLAY_H_
 
 //WRITE YOUR FUNCTIONS HERE
+#include <avr/io.h>
+#include <stdlib.h>
+
+typedef struct display * display_t;
+
+display_t display_create(volatile uint8_t *port);
+void destroy_display(display_t display);
+
+void light_up_led(display_t self, int led_no);
+void light_off_led(display_t self, int led_no);
+void light_up_no(display_t self, int no);
+void light_off_all(display_t self);
 
 #endif
