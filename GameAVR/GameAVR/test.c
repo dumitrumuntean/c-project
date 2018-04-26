62b8b3573395222367e7c942285a4e83823bfedf
Andrew Ngwira <260008@via.dk>
2018-04-26 09:49:35 +0200

    display Test

--- GameAVR/GameAVR/test.c
+++ GameAVR/GameAVR/test.c
@@ -3,4 +3,24 @@
  *
  * Created: 19/04/2018 09:05:20
  *  Author: Dima
- */ 
+ */
+
+ 
+ #include <avr/io.h>
+ #include <stdio.h>
+ #include <avr/interrupt.h>
+ #include "atmega2560_drivers.h"
+ #include "minunit.h"
+ #include "game.h"
+ 
+ int tests_run = 0;
+ 
+ static char * display_create(&PORTA)
+ {
+	 DDRA = 0Xff;
+	 display_t result = display_create(&PORTA);
+	  mu_assert(display_create("display_create(&PORTA) == null"), result != NULL );
+	  return 0;
+ } 
+ 
+ 
